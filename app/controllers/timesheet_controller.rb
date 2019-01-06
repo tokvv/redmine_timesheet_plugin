cae38c258c7d680d9ba208c1b79a4ddd4132ab60
tokvvdb <v.tokarchuk@dev-branch.com>
2019-01-06 16:44:51 +0200

    Fix reset date after click apply.

--- app/controllers/timesheet_controller.rb
+++ app/controllers/timesheet_controller.rb
@@ -155,8 +155,8 @@ class TimesheetController < ApplicationController
 
     if timesheet
       session[SessionKey] ||= {}
-      session[SessionKey]['date_from'] = timesheet.date_from
-      session[SessionKey]['date_to'] = timesheet.date_to
+      session[SessionKey]['date_from'] = timesheet_date_from
+      session[SessionKey]['date_to'] = timesheet_date_to
     end
   end
 
